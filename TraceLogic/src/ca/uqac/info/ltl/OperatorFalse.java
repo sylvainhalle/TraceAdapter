0c012bc60faa38e0bd26b9bb2f1aa618a492e784
Sylvain Hallé <shalle@acm.org>
2012-05-29 12:45:53 -0400

    Ajout constantes true et false

---
+++ TraceLogic/src/ca/uqac/info/ltl/OperatorFalse.java
@@ -0,0 +1,12 @@
+package ca.uqac.info.ltl;
+
+public class OperatorFalse extends Atom
+{
+	private static final String SYMBOL = "\u22A5";
+
+	public OperatorFalse()
+	{
+		super();
+		m_symbol = SYMBOL;
+	}
+}
---
+++ TraceLogic/src/ca/uqac/info/ltl/OperatorTrue.java
@@ -0,0 +1,12 @@
+package ca.uqac.info.ltl;
+
+public class OperatorTrue extends Atom
+{
+	private static final String SYMBOL = "\u22A4";
+
+	public OperatorTrue()
+	{
+		super();
+		m_symbol = SYMBOL;
+	}
+}
